7fdc827c1796eca151a287f72bd625ee5bd635c6
SainathChowdaryK <sainathchowdary666@gmail.com>
2022-06-13 12:54:55 +0530

    Code updated

--- src/main/java/com/demo/configclient/UserClientApplication.java
+++ src/main/java/com/demo/configclient/UserClientApplication.java
@@ -2,10 +2,8 @@ package com.demo.configclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
-@RefreshScope
 public class UserClientApplication {
 
 	public static void main(String[] args) {
